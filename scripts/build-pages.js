e2a4ffe91b2aed3fbcf5e3e2c841849d717f2ebf
OllieinCanada <ollieincanada@users.noreply.github.com>
2026-07-01 12:06:33 -0400

    Fix GitHub Pages static build

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -2,11 +2,20 @@ const fs = require("fs");
 const path = require("path");
 const { copyDir, ensureDir, npmCommand, removeDir, rootPath, run } = require("./_helpers");
 
-const buildDir = rootPath("build");
+const buildDirName = "build-pages-output";
+const buildDir = rootPath(buildDirName);
 const docsDir = rootPath("docs");
+const productionApiBase = process.env.REACT_APP_API_BASE_URL || "https://myaipa-api.onrender.com";
 
 console.log("Building production React app...");
-run(npmCommand(), ["run", "build"]);
+removeDir(buildDir);
+run(npmCommand(), ["run", "build"], {
+  env: {
+    BUILD_PATH: buildDirName,
+    REACT_APP_API_BASE_URL: productionApiBase,
+    REACT_APP_CHECKOUT_API_BASE_URL: process.env.REACT_APP_CHECKOUT_API_BASE_URL || productionApiBase,
+  },
+});
 
 if (!fs.existsSync(buildDir)) {
   throw new Error("Build folder was not created.");
@@ -18,5 +27,6 @@ ensureDir(docsDir);
 // Only remove generated static assets. Keep extra docs/audio/transcript files intact.
 removeDir(path.join(docsDir, "static"));
 copyDir(buildDir, docsDir);
+fs.writeFileSync(path.join(docsDir, ".nojekyll"), "");
 
 console.log("Done. docs/ now contains the latest production build.");
